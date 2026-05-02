9bf1fd03b50735bc9deda7d3831e93910560b763
Zemphis <fardeen.15.l1f3@gmail.com>
2026-05-02 21:59:11 +0930

    adapted to include different types of train sections

--- InterlockingImpl.java
+++ InterlockingImpl.java
@@ -3,23 +3,33 @@ import java.util.*;
 public class InterlockingImpl {
     private static final Set<Integer> ALL_SECTIONS =
             new HashSet<> (Arrays.asList(1,2,3,4,5,6,7,8,9,10,11));
+    private static final Set<Integer> FREIGHT_SECTIONS =
+            new HashSet<> (Arrays.asList(3,7,11));
+    private static final Set<Integer> PASSENGERS_SECTIONS =
+            new HashSet<> (Arrays.asList(1,2,4,5,6,8,9,10));
+    private static final Set<Integer> SB_ENTRIES =
+            new HashSet<> (Arrays.asList(1,3));
+    private static final Set<Integer> SB_EXITS =
+            new HashSet<> (Arrays.asList(4,8,9,11));
+    private static final Set<Integer> NB_ENTRIES =
+            new HashSet<> (Arrays.asList(4,9,10,11));
+    private static final Set<Integer> NB_EXITS =
+            new HashSet<> (Arrays.asList(2,3));
 
     private static final Map<Integer, List<Integer>> NEXT = new HashMap<>();
 
     static {
-        NEXT.put(1, Arrays.asList(2));
-        NEXT.put(2, Arrays.asList(3));
-
-        NEXT.put(3, Collection.emptyList()); // east exit
+        NEXT.put(1, Arrays.asList(5));
+        NEXT.put(2, Arrays.asList(6));
+        NEXT.put(3, Arrays.asList(7));
         NEXT.put(4, Arrays.asList(5));
-        NEXT.put(5, Arrays.asList(6, 10)); // main line preferred
-
-        NEXT.put(6, Collections.emptyList()); // east exit
-        NEXT.put(7, Arrays.asList(8));
-        NEXT.put(8, Arrays.asList(9));
-
-        NEXT.put(9, Collections.emptyList());
+        NEXT.put(5, Arrays.asList(4,8,9,1,2));
+        NEXT.put(6, Arrays.asList(9,10,2));
+        NEXT.put(7, Arrays.asList(11,3));
+        NEXT.put(8, Arrays.asList(5));
+        NEXT.put(9, Arrays.asList(5,6));
         NEXT.put(10, Arrays.asList(6));
+        NEXT.put(11, Arrays.asList(7));
     }
 
     private static final Set<Integer> ENTRY_SECTIONS = new HashSet<>(Arrays.asList(1,4,7));
